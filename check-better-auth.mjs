1c06d44344ea777c25c8a22e797f9d59df5b6f00
Blake Harms <blake@harms.haus>
2026-01-16 16:04:27 -0600

    chore: remove temporary better-auth client check script.

--- check-better-auth.mjs
+++
@@ -1,5 +0,0 @@
-import pkg from "better-auth/client";
-console.log("Exports from better-auth/client:", Object.keys(pkg));
-console.log("useSession:", pkg.useSession);
-console.log("signIn:", pkg.signIn);
-console.log("signUp:", pkg.signUp);
